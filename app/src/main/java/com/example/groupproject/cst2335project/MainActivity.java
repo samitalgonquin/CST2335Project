4396be59863c4a3463b6c6e75df9655aead956e4
samitalgonquin <samit8nov@gmail.com>
2019-11-14 10:40:44 -0500

    Changed Main activity screen layout. Created a branch named ElectricCarCharging.

--- app/src/main/java/com/example/groupproject/cst2335project/MainActivity.java
+++ app/src/main/java/com/example/groupproject/cst2335project/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.groupproject.cst2335project;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    Button btnUpdate, btnAbout, btnCharging;
+    final Context context = this;
+    String info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +30,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        btnCharging = findViewById(R.id.btnCharging);
+        btnCharging.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent goToChargingStationFinderIntent = new Intent(MainActivity.this,ChargingStationFinder.class);
+                startActivity(goToChargingStationFinderIntent);
+            }
+        });
+
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"You already have the latest version of the app.",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnAbout = findViewById(R.id.btnAbout);
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                ViewGroup viewGroup = findViewById(android.R.id.content);
+                View dialogView = LayoutInflater.from(v.getContext()).inflate(R.layout.custom_dialog, viewGroup, false);
+                builder.setView(dialogView);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+        
     }
+
 }
